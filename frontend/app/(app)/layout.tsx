605989cb5dbe97507a733a3adc1cbfcae730af2f
Jomel <jomelbatan6@gmail.com>
2026-08-02 17:58:17 +0800

    Layout Added

---
+++ frontend/app/(app)/layout.tsx
@@ -0,0 +1,15 @@
+import { LayoutInner } from "@/components/layout/AppLayout";
+import { BoardProvider } from "@/context/BoardContext";
+import React from "react";
+
+interface AppLayout {
+  children: React.ReactNode;
+}
+
+export default async function AppLayout({ children }: AppLayout) {
+  return (
+    <BoardProvider>
+      <LayoutInner>{children}</LayoutInner>
+    </BoardProvider>
+  );
+}

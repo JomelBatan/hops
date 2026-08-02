01c8d623de2e814db4524eee58492f5c3ce86f60
Jomel <jomelbatan6@gmail.com>
2026-08-02 16:58:04 +0800

    Routing Done

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -1,12 +1,19 @@
-"use client";
 import { LayoutInner } from "@/components/layout/AppLayout";
 import { BoardProvider } from "@/context/BoardContext";
 import React from "react";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 interface AppLayout {
   children: React.ReactNode;
 }
-export default function AppLayout({ children }: AppLayout) {
+
+export default async function AppLayout({ children }: AppLayout) {
+  const token = (await cookies()).get("access_token");
+
+  if (!token) {
+    redirect("/login");
+  }
   return (
     <BoardProvider>
       <LayoutInner>{children}</LayoutInner>
